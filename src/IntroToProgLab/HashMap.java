72da8b2568655eda6c9f3f2b442d5b085f07916e
Yaroslav Obruch <Stamin4ik4@gmail.com>
2019-03-19 16:06:45 +0200

    create basic classes in project

---
+++ src/IntroToProgLab/HashMap.java
@@ -0,0 +1,16 @@
+package IntroToProgLab;
+
+public class HashMap {
+    final int defaultCapacity=16;
+    private int [] arrayOfIndexs;
+    public HashMap(int size){
+        arrayOfIndexs=new int[size];
+    }
+    public HashMap(){
+        arrayOfIndexs=new int[defaultCapacity];
+    }
+    public void put(Node node){
+        Node element=node;
+
+    }
+}
---
+++ src/IntroToProgLab/Main.java
@@ -0,0 +1,6 @@
+package IntroToProgLab;
+
+public class Main{
+    public static void main(String[] args) {
+    }
+}
